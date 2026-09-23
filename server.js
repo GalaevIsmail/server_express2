289f9550efbb3d67c9695f6907a4f0e29a06f1ef
Галаев Исмаил У <student115@ithub.local>
2026-09-23 11:54:14 +0300

    Добавлени данныых на сайт

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');//подключили библиотеку E
 const { log } = require('console');
 const app = express()// запустили Express
 
+
+app.use(express.json())
+
 const PORT = 3000;//Обозначили порт для дальнейшего запуска
 
 app.get('/', (req, res)=>{/// создаем git запрос для получения главной сьраницы
@@ -23,9 +26,25 @@ app.get('/fon/:number' , (req, res)=>{
 })
 
 app.get('/product/:id',(req, res)=>{
-    console.log(req.params)
     res.send(`Товар с id: ${req.params.id}`)
+});
+
+app.post('/product/add', (req, res)=>{
+    const {id, title, price} = req.body
+  res.send(`Товар добавлен успешно 
+    Название:${title} 
+    цена:${price}`);
+    
+    
 })
+app.post('/regitre/regiteres' , (req , res)=>{
+    const {name,lasname,numberhone } = req.body
+    res.send(`Регистрация прошла успешна
+        Имя:${name} 
+        Фамилия :${lasname} 
+        Номер телефона:${numberhone}`)
+})
+
 app.listen(PORT, ()=>{//настраиваем сервер на чем он будет запущен
     console.log(`Сервер успешно запушен по адресу: 127.0.0.1:${PORT}`)
 })
